925b6f75b2529d7d23ccd9df6daa2532ff2c9a9a
Julianna-M27 <juliannamorena1730@gmail.com>
2025-03-17 10:42:41 -0700

    fixed font sizing on welcome page

--- cirkle-app/app/page.tsx
+++ cirkle-app/app/page.tsx
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#FAF3E9] flex flex-col items-center justify-center p-8 text-center">
       {/* Large Heading */}
-      <h1 className="text-7xl font-bold text-[#cda584] leading-tight">
+      <h1 className="text-9xl font-bold text-[#cda584] leading-tight">
         Welcome To <br /> Cirkle!
       </h1>
 
       {/* Subtitle */}
-      <p className="mt-4 text-2xl font-semibold text-[#A98973] tracking-wide">
+      <p className="mt-4 text-4xl font-semibold text-[#A98973] tracking-wide">
         COLLABORATIVE STUDY FOR{" "}
         <span className="text-[#924747] italic">'BETTER RESULTS'</span>
       </p>
@@ -20,8 +20,8 @@ export default function Home() {
       
       <div className="mt-12">
       <Link href="/welcome">
-        <Button className="flex items-center space-x-3 bg-[#924747] hover:bg-[#924747]/90 text-white px-6 py-3 rounded-full text-xl">
-          <img src="/google.png" alt="Google Icon" className="text-2xl h-5 w-5"/>
+        <Button className="flex items-center space-x-3 bg-[#924747] hover:bg-[#924747]/90 text-white px-8 py-5 rounded-full text-xl">
+          <img src="/google.png" alt="Google Icon" className="text-4xl h-6 w-6"/>
           <span>Login with Google</span>
         </Button>
         </Link>
